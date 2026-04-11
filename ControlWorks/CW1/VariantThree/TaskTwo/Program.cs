4e998babad6ab9d4e0f1bfdd0cdf2e9b261ea097
IsmailEmir <ismail.emir.koch@gmail.com>
2026-04-11 15:40:15 +0300

    CW1 Var3

---
+++ ControlWorks/CW1/VariantThree/TaskTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskTwo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sensor temperatureSensor = new Sensor();
+        temperatureSensor.Alert += (sender, e) =>
+        {
+            Console.WriteLine($"Внимание! Изменение на {Math.Round(e.PercentChange, 2)}%! Старое значение: {e.OldValue}, Новое: {e.NewValue}");
+        };
+        temperatureSensor.SetValue(20.0);
+        temperatureSensor.SetValue(22.5);
+    }
+}
