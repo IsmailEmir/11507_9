48c96706d5ab35030cb8f7e9b7b6e7ef9dc861aa
Коч Исмаил Эмир <101726915+IsmailEmir@users.noreply.github.com>
2026-04-11 21:36:54 +0300

    Delete ControlWorks/CW1 directory

--- ControlWorks/CW1/temp/TaskTwo/SensorEvents.cs
+++
@@ -1,8 +0,0 @@
-namespace TaskTwo;
-
-public class SensorEventArgs : EventArgs
-{
-    public double OldValue { get; set; }
-    public double NewValue { get; set; }
-    public double PercentChange { get; set; }
-}
